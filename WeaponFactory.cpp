b8aa97ac2e9b9ee23f4987a9e3c8352529e886e4
Elisa Noelle Wheaton <wheaton@linprog1.cs.fsu.edu>
2019-02-03 23:36:01 -0500

    Files updated to support new Weapon classes
    The information files have been updated to describe the current state of
    the repository.
    WeaponFactory.cpp function Weapon * WeaponFactory::getWeapon(std::string name)
    and main updated to support new Weapon classes, SimpleHammer, CrazyRandomSword,
    and TheseHands.

--- WeaponFactory.cpp
+++ WeaponFactory.cpp
@@ -1,13 +1,13 @@
-/* 
+/*
  * File:   WeaponFactory.cpp
  * Author: Javier <jrescobara@gmail.com>
- *  
- */ 
+ *
+ */
 
 #include <string>
-#include <stddef.h> 
+#include <stddef.h>
 #include "WeaponFactory.h"
-#include "CommonSword.h" 
+#include "CommonSword.h"
 #include "CommonSpear.h"
 
 WeaponFactory* WeaponFactory::instance = NULL;
@@ -23,9 +23,22 @@ Weapon * WeaponFactory::getWeapon(std::string name) {
     if (name.compare("sword") == 0) {
         return new CommonSword();
     }
-    
+
     if (name.compare("spear") == 0) {
         return new CommonSpear();
+    }
+    
+//new weapon function calls added below
+    if (name.compare("hammer") == 0) {
+        return new SimpleHamer();
+    }
+
+    if (name.compare("random") == 0) {
+        return new CrazyRandomSword();
+    }
+
+    if (name.compare("hands") == 0) {
+        return new TheseHands();
     }
 
     throw "Invalid weapon";
