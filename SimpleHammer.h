0d3c318809b93696d932e0185a970de7ec12a38d
Elisa Noelle Wheaton <wheaton@linprog1.cs.fsu.edu>
2019-02-03 22:21:18 -0500

    Update code in SimpleHammer.h
    The information files have been updated to describe the current state of
    the repository.
    SimpleHammer.h was updated to include ifndef tags and code with matching format
    to the other weapon classes was added(CommonSpear and CommonSword) including
    constructor and class functions.

--- SimpleHammer.h
+++ SimpleHammer.h
@@ -1,6 +1,27 @@
 /*
  * File:   SimpleHammer.h
- * Author: Elisa Wheaton <wheaton@cs.fsu.edu> 
+ * Author: Elisa Wheaton <wheaton@cs.fsu.edu>
  *
  * Created on 2/3/19  9:54 PM
  */
+
+ #include <string>
+ #include "Weapon.h"
+
+ #ifndef SIMPLEHAMMER_H
+ #define SIMPLEHAMMER_CPP
+
+ class SimpleHammer : public Weapon {
+ public:
+
+     SimpleHammer() : Weapon("Simple Hammer", 25.0) //Calls Weapon(name, hitpoints) constructor with values Simple Hammer and 25
+     {
+     }
+
+     virtual ~SimpleHammer() {};
+
+     virtual double hit(double armor);
+
+ };
+
+ #endif /* SIMPLEHAMMER_H */
