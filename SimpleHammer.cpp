e3e83d5bb179e6cabe84222357604ef5d8f5f468
Elisa Noelle Wheaton <wheaton@linprog1.cs.fsu.edu>
2019-02-03 22:39:57 -0500

    Class function hit(double armor) written for SimpleHammer.cpp
    The information files have been updated to describe the current state of
    the repository.

--- SimpleHammer.cpp
+++ SimpleHammer.cpp
@@ -4,3 +4,18 @@
  *
  * Created on 2/3/19  9:54 PM
  */
+ #include "SimpleHammer.h"
+ double CommonSword::hit(double armor){
+     double damage;
+
+     if(armor < 30)
+       damage = hitpoints;
+
+     if(damage < 0)
+       damage = 0;
+
+     else
+       damage = hitPoints - armor;
+     
+     return damage;
+ }
