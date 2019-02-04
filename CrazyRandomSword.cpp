823a9545f0cfac3d2731e600c5eea540e85036c5
Elisa Noelle Wheaton <wheaton@linprog1.cs.fsu.edu>
2019-02-03 22:54:50 -0500

    Created and added comments to CrazyRandomSword class files
    Information files have been updated to reflect the current content of the
    local repository.
    SimpleHammer.cpp and SimpleHammer.h were created and comments with class
    info, ifndefs, and #includes were added.

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,13 @@
+/*
+ * File:   CrazyRandomSword.cpp
+ * Author: Elisa Wheaton <wheaton@cs.fsu.edu>
+ *
+ * Created on 2/3/19  10:49 PM
+ */
+ #include "CrazyRandomSword.h"
+
+/*
+hitpoints = random integer number between 7 and
+100. Ignores a random amount of integer armor points, ranging from to 2 to a
+third of the armor the weapon hits.
+*/
---
+++ CrazyRandomSword.h
@@ -0,0 +1,19 @@
+/*
+ * File:   CrazyRandomSword.cpp
+ * Author: Elisa Wheaton <wheaton@cs.fsu.edu>
+ *
+ * Created on 2/3/19  10:49 PM
+ */
+ #include <string>
+ #include "Weapon.h"
+
+ #ifndef CRAZYRANDOMSWORD_H
+ #define CRAZYRANDOMSWORD_H
+
+/*
+hitpoints = random integer number between 7 and
+100. Ignores a random amount of integer armor points, ranging from to 2 to a third of
+the armor the weapon hits.
+*/
+
+ #endif /* CRAZYRANDOMSWORD_H */
